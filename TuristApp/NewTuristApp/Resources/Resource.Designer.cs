4ebc52b5631b90c53e9db19dcc7cf66a43c043e3
Jochira <ssjgogito@live.se>
2016-04-11 11:00:50 +0200

    MenuView-knapparna är fixade.

--- TuristApp/NewTuristApp/Resources/Resource.Designer.cs
+++ TuristApp/NewTuristApp/Resources/Resource.Designer.cs
@@ -63,14 +63,14 @@ namespace NewTuristApp
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050002
+			public const int button1 = 2131034114;
+			
 			// aapt resource value: 0x7f050003
-			public const int button1 = 2131034115;
+			public const int button2 = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int button2 = 2131034116;
-			
-			// aapt resource value: 0x7f050002
-			public const int button3 = 2131034114;
+			public const int button3 = 2131034116;
 			
 			// aapt resource value: 0x7f050005
 			public const int button4 = 2131034117;
